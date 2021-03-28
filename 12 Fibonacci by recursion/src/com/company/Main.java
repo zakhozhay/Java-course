5183792f435a4730b1cc0e1b3694926103243bd3
Oleh Zakhozhai <zakhozhay.oleg@gmail.com>
2021-03-28 14:46:54 +0300

    Fibonacci project was added

---
+++ 12 Fibonacci by recursion/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int fibonacci(int n){
+        int result;
+        int fibonacci1;
+        int fibonacci2;
+        System.out.println("Call function fibonacci(" + n + ")...");
+        if (n < 3)
+        {
+            result = 1;
+        }
+        else
+        {
+            fibonacci1 = fibonacci(n - 1);
+            fibonacci2 = fibonacci(n - 2);
+            result = fibonacci1 + fibonacci2;
+        }
+        System.out.println("Result of fibonachi(" + n + ") = " + result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please, enter number of Fibonacci element: ");
+        int number = input.nextInt();
+        System.out.println(number + "th element of Fibonacci is " + fibonacci(number));
+    }
+}
